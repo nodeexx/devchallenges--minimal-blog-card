e223462d8b5f9bce6446cb8cde15aa3c59d4cab0
nodeexx <156369338+nodeexx@users.noreply.github.com>
2024-04-20 15:43:36 +0200

    Setup shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
+<script>
+import { Button } from '$lib/browser/shadcn-svelte/components/ui/button';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p class="text-green-500">
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+<Button>Click me</Button>
 
 <style lang="postcss">
 h1 {
