1d084086f26cc6249b5ecfcc662269d3d080bc12
nodeexx <156369338+nodeexx@users.noreply.github.com>
2024-04-20 14:51:00 +0200

    Setup PostCSS and Stylelint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style lang="postcss">
+h1 {
+  color: blue;
+
+  &:hover {
+    color: red;
+  }
+}
+</style>
